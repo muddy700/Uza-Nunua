c4c52c1435e9b56deb28adca73300b8331a42bed
Muddy700 <mohamedmfaume700@gmail.com>
2020-12-09 13:40:49 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { Container} from './components/container'
+const  App = () => {
+  return (
+    <div className="App">
+        <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography} from 'antd'
+
+export const Container = () => {
+    const { Title } = Typography
+    return (
+        <div>
+            <Title>Header</Title>
+        </div>
+    )
+}
+
